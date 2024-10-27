26252c44ea411b276e2bee73ef339db9e25ba562
Wolfgang Zimmermann <wolfgang@zimmermann.red>
2024-10-27 15:44:40 +0100

    fix: correctly use null byte

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -45,7 +45,7 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
-	case '0':
+	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
 	}
